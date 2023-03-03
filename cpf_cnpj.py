ac9a20a58670824bc72f2181c2931176ad7dffed
joaodamasio <125613915+joaodamasio@users.noreply.github.com>
2023-03-03 09:19:31 -0300

    Add files via upload

---
+++ cpf_cnpj.py
@@ -0,0 +1,45 @@
+from validate_docbr import CPF,CNPJ
+
+class Documento:
+    @staticmethod
+    def cria_documento(documento):
+        if len(documento) == 11:
+            return DocCpf(documento)
+        elif len(documento) == DocCnpj:
+            return DocCnpj(documento)
+        else:
+            raise ValueError ("Quantidade de caracteres inválida.")
+class DocCpf:
+    
+    def __init__(self,documento):
+        if self.valida(documento):
+            self.cpf = documento
+        else:
+            raise ValueError ("CPF inválido")
+        
+    def __str__(self):
+        return self.format()
+
+    def valida(self,documento):
+        if len(documento) == 11:
+            validador = CPF()
+            return validador.validate(documento)
+    
+    def format(self):
+        mascara = CPF()
+        return mascara.mask(self.cpf)
+
+class DocCnpj:
+    def __init__(self,documento):
+        if self.valida(documento):
+            self.cnpj = documento
+        else:
+            raise ValueError ("Quantidade de caracteres inválida")
+
+    def valida(self,documento):
+        validate_cnpj = CNPJ()
+        return validate_cnpj.validate(documento)
+    
+    def format(self):
+        mascara = CNPJ()
+        return mascara.mask(self.cnpj)
